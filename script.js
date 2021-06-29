aa3ed419c45098d49e940cf2a958c6bddbf60be8
M603-droid <mridulanamdeo@gmail.com>
2021-06-29 16:56:19 +0200

    extras

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-let changeTheText = function(){
+
+// <!--Write a function to change the h1 text -->
+let changText = function(){
     let changeTheText = document.getElementById("new-day").innerText = "The shop is closed"
-    return changeTheText
+    return changeText
 }
-changeTheText()
+//changeTheText()
+
+// <!-- EX8.: Write a function to change the page background color -->
+
+let changeColorOfBody = function(){
+document.querySelector('body').style.backgroundColor = "green"
+
+
+
+}
+
+
+// <!-- EX9.: Write a function to change the footer address with a fake one -->
+    
+
+    
+// <!-- EX10.: Write a function to add a CSS class to every Amazon link -->
+// <!-- EX11: Write a function to toggle a CSS class for all the images in the table; that class should set the visibility of the image -->
+// <!-- EX12: Write a function to color the price of each product in a different one every time it's invoked -->
